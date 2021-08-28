484ec5a8eaa1e4cf30bbecdb34ffff48e23b29f6
Sibints2000 <sibints2000@gmail.com>
2021-08-28 19:42:53 +0530

    Thirty First Commit

--- ui.py
+++ ui.py
@@ -11,7 +11,7 @@ class QuizInterface:
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
 
-        self.score_label = Label(text="Score: 0", fg="white")
+        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
         self.score_label.grid(row=0, column=1)
 
 

f725cf9a044dd27509e584d70037ef5ab95f0a49
Sibints2000 <sibints2000@gmail.com>
2021-08-28 20:34:43 +0530

    Sixty Third Commit

--- ui.py
+++ ui.py
@@ -40,8 +40,8 @@ class QuizInterface:
         self.window.mainloop()
 
     def get_next_question(self):
+        self.canvas.config(bg="white")
         if self.quiz.still_has_questions():
-            self.canvas.config(bg="white")
             self.score_label.config(text=f"Score: {self.quiz.score}")
             q_text = self.quiz.next_question()
             self.canvas.itemconfig(self.question_text, text=q_text)
